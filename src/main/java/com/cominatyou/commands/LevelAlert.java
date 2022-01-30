3ae91de7c372f13db80bd9146076a345db891b67
William M <cominatyou@cominatyou.com>
2022-01-30 01:35:33 -0600

    Fix LevelAlert command

--- src/main/java/com/cominatyou/commands/LevelAlert.java
+++ src/main/java/com/cominatyou/commands/LevelAlert.java
@@ -3,21 +3,27 @@ package com.cominatyou.commands;
 import com.cominatyou.db.RedisInstance;
 import com.cominatyou.util.Values;
 
+import org.javacord.api.entity.channel.ChannelType;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 public class LevelAlert {
+    private final static String alertsDisabledMessage = "Alright! Level alerts have been disabled for you. If you want to turn them back on in the future, run this command again.";
+    private final static String alertsEnabledMessage = "Alright! Level alerts have been enabled for you. If you want to turn them off in the future, run this command again.";
+
     public static void setPreference(MessageCreateEvent message) {
-        if (message.getChannel().getId() != Values.BOT_CHANNEL) return;
+        if (message.getChannel().getId() != Values.BOT_CHANNEL && message.getChannel().getType() != ChannelType.PRIVATE_CHANNEL) return; // TODO: Clear this for testing
         final String userId = message.getMessageAuthor().getIdAsString();
         final boolean userPreference = RedisInstance.getBoolean("users:" + userId + ":levelalertsdisabled");
 
-        if (userPreference) {
+        if (!userPreference) {
             RedisInstance.getInstance().set("users:" + userId + ":levelalertsdisabled", "true");
-            message.getMessage().reply("Alright! Level alerts have been disabled for you. If you want to turn them back on in the future, run this command again.");
+            if (message.getChannel().getType() == ChannelType.PRIVATE_CHANNEL) message.getChannel().sendMessage(alertsDisabledMessage);
+            else message.getMessage().reply(alertsDisabledMessage);
         }
         else {
             RedisInstance.getInstance().set("users:" + userId + ":levelalertsdisabled", "false");
-            message.getMessage().reply("Alright! Level alerts have been enabled for you. If you want to turn them off in the future, run this command again.");
+            if (message.getChannel().getType() == ChannelType.PRIVATE_CHANNEL) message.getChannel().sendMessage(alertsEnabledMessage);
+            else message.getMessage().reply(alertsEnabledMessage);
         }
     }
 }
