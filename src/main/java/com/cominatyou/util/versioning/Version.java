0c76755b363472c9aebad9704f49a1842b8de736
William M <cominatyou@cominatyou.com>
2022-02-20 14:18:10 -0600

    Bump versions

--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,6 +1,6 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "1.1.6";
-    public static final String BUILD_NUMBER = "10A051";
+    public static final String VERSION = "1.1.7";
+    public static final String BUILD_NUMBER = "10A052";
 }
