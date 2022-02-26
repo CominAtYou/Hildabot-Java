1a0f800bce3db718adcc86a22cd2efd99e221a64
William M <cominatyou@cominatyou.com>
2022-02-25 20:26:03 -0600

    Revert Say command

--- src/main/java/com/cominatyou/commands/admin/Say.java
+++ src/main/java/com/cominatyou/commands/admin/Say.java
@@ -2,46 +2,17 @@ package com.cominatyou.commands.admin;
 
 import java.util.List;
 
-import com.cominatyou.util.Values;
-
-import org.javacord.api.entity.message.MessageBuilder;
-import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 public class Say {
     public static void run(MessageCreateEvent message, List<String> messageArgs) {
         if (!message.getMessageAuthor().isBotOwner()) return;
-
-        final String messageString = String.join(" ", messageArgs);
-        final EmbedBuilder embed = new EmbedBuilder().setColor(new java.awt.Color(Values.HILDA_BLUE));
-        final MessageBuilder messageBuilder = new MessageBuilder();
-
-        if (messageString.contains("_t:") && messageString.contains("_b:")) {
-            embed.setTitle(messageString.substring(messageString.indexOf("_t:") + 3, messageString.indexOf("_b:")));
-            embed.setDescription(messageString.substring(messageString.indexOf("_b:") + 3));
-            messageBuilder.setContent(messageString.substring(0, messageString.indexOf("_t:")));
-            messageBuilder.setEmbed(embed);
-        }
-        else if (messageString.contains("_t:")) {
-            embed.setTitle(messageString.substring(messageString.indexOf("_t:") + 3));
-            messageBuilder.setContent(messageString.substring(0, messageString.indexOf("_t:")));
-            messageBuilder.setEmbed(embed);
-        }
-        else if (messageString.contains("_b:")) {
-            embed.setTitle(messageString.substring(messageString.indexOf("_b:") + 3));
-            messageBuilder.setContent(messageString.substring(0, messageString.indexOf("_:")));
-            messageBuilder.setEmbed(embed);
-        }
-        else {
-            messageBuilder.setContent(messageString);
-        }
-
         message.getMessage().getReferencedMessage().ifPresentOrElse(replyMessage -> { // Reply to the replied-to message if the invoking message was in reply to another.
             message.deleteMessage();
-            messageBuilder.replyTo(replyMessage);
+            replyMessage.reply(String.join(" ", messageArgs));
         }, () -> {
             message.deleteMessage();
-            messageBuilder.send(message.getChannel());
+            message.getChannel().sendMessage(String.join(" ", messageArgs));
         });
     }
 }
--- src/main/java/com/cominatyou/eventhandlers/MessageCreate.java
+++ src/main/java/com/cominatyou/eventhandlers/MessageCreate.java
@@ -3,6 +3,7 @@ package com.cominatyou.eventhandlers;
 import com.cominatyou.Config;
 import com.cominatyou.TextCommandHandler;
 import com.cominatyou.util.MessageValidity;
+import com.cominatyou.util.Values;
 import com.cominatyou.xp.XPSystem;
 
 import org.javacord.api.entity.channel.ChannelType;
@@ -11,7 +12,7 @@ import org.javacord.api.event.message.MessageCreateEvent;
 public class MessageCreate {
     public static void route(MessageCreateEvent event) {
         if (!MessageValidity.test(event)) return;
-        if (!event.getMessageContent().startsWith(Config.PREFIX) && event.getChannel().getType() == ChannelType.SERVER_TEXT_CHANNEL) {
+        if (!event.getMessageContent().startsWith(Config.PREFIX) && event.getChannel().getType() == ChannelType.SERVER_TEXT_CHANNEL && event.getServer().get().getId() == Values.HILDACORD_ID) {
             XPSystem.giveXPForMessage(event);
         }
         else {
