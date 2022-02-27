b159936366b74aad6b9b7685308a20ba8dea9db0
William M <cominatyou@cominatyou.com>
2022-02-27 14:56:46 -0600

    Add Config.java

---
+++ src/main/java/com/cominatyou/Config.java
@@ -0,0 +1,8 @@
+package com.cominatyou;
+
+public class Config {
+    public static final String PREFIX = "";
+    public static final String TOKEN = "";
+    public static final String REDIS_INSTANCE_ADDRESS = "";
+
+}
