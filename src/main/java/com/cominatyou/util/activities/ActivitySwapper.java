4145a49588facbeb1200b98e951949db096b09f0
William M <cominatyou@cominatyou.com>
2022-03-02 10:35:30 -0600

    Add activity

--- src/main/java/com/cominatyou/util/activities/ActivitySwapper.java
+++ src/main/java/com/cominatyou/util/activities/ActivitySwapper.java
@@ -13,11 +13,13 @@ public class ActivitySwapper {
     private static final Activity[] activities = { new Activity(ActivityType.PLAYING, "with Twig"),
             new Activity(ActivityType.WATCHING, "the Woffs migrate"),
             new Activity(ActivityType.WATCHING, "for your submissions!"),
-            new Activity(ActivityType.PLAYING, "with the Sparrow Scouts"),
+            new Activity(ActivityType.WATCHING, "Frida practice magic"),
             new Activity(ActivityType.WATCHING, "Hilda and the Mountain King"),
+            new Activity(ActivityType.PLAYING, "with the Sparrow Scouts"),
             new Activity(ActivityType.PLAYING, "with the trolls"),
-            new Activity(ActivityType.COMPETING, "a dodgeball game"),
-            new Activity(ActivityType.PLAYING, String.format("Version %s / Build %s", Version.VERSION, Version.BUILD_NUMBER)) };
+            new Activity(ActivityType.PLAYING, String.format("Version %s / Build %s", Version.VERSION, Version.BUILD_NUMBER)),
+            new Activity(ActivityType.COMPETING, "a dodgeball game")
+    };
 
     public static void start(DiscordApi client) {
         new Timer().scheduleAtFixedRate(new TimerTask() {
--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,6 +1,6 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "1.3.0";
-    public static final String BUILD_NUMBER = "10C078";
+    public static final String VERSION = "1.3.1";
+    public static final String BUILD_NUMBER = "10C079";
 }
