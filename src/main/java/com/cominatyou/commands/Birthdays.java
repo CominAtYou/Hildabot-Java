5255fb01fd0bb9937e3d4170267587bf85e97c5a
William M <cominatyou@cominatyou.com>
2022-03-06 00:14:36 -0600

    Add example to birthday command

--- src/main/java/com/cominatyou/commands/Birthdays.java
+++ src/main/java/com/cominatyou/commands/Birthdays.java
@@ -22,7 +22,7 @@ public class Birthdays {
 
     public static void run(MessageCreateEvent message, List<String> messageArgs) {
         if (messageArgs.size() == 0 || messageArgs.size() == 1 && !messageArgs.get(0).equalsIgnoreCase("list")) {
-            message.getMessage().reply("Looks like you're missing some arguments. Please make sure you provided a command (set|edit) and a date, or list and a month if you want to view upcoming birthdays!");
+            message.getMessage().reply("Looks like you're missing some arguments. Please make sure you provided a command (set|edit) and a date (i.e. `h!birthday set 06-21`), or list and a month if you want to view upcoming birthdays!");
         }
         else if (messageArgs.get(0).equalsIgnoreCase("set")) {
             Birthdays.set(message, messageArgs);
--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,6 +1,6 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "1.3.4";
-    public static final String BUILD_NUMBER = "10C082";
+    public static final String VERSION = "1.3.5";
+    public static final String BUILD_NUMBER = "10C083";
 }
