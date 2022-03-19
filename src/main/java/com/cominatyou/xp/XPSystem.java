e36d3a804816bdb4720295946dcf00a5dbdbf324
William M <cominatyou@cominatyou.com>
2022-03-19 13:49:04 -0500

    Moderation stuff

--- src/main/java/com/cominatyou/xp/XPSystem.java
+++ src/main/java/com/cominatyou/xp/XPSystem.java
@@ -22,6 +22,8 @@ public class XPSystem {
 
         final RedisUserEntry user = new RedisUserEntry(message.getMessageAuthor());
 
+        if (RedisInstance.getInstance().lrange("config:xp:disallowedusers", 0, -1).contains(user.getIdAsString())) return;
+
         // XP will only be granted for 7 messages sent within 30 seconds. This might
         // need to be increased.
         if (user.getInt("recentmessagecount") == 7) return;
