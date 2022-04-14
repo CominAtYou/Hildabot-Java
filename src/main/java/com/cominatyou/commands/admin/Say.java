6ec00ec6d1374e723301f714566157507b4edf2d
William M <cominatyou@cominatyou.com>
2022-04-13 21:02:16 -0500

    Add typing for h!say

--- src/main/java/com/cominatyou/commands/admin/Say.java
+++ src/main/java/com/cominatyou/commands/admin/Say.java
@@ -7,6 +7,13 @@ import org.javacord.api.event.message.MessageCreateEvent;
 public class Say {
     public static void run(MessageCreateEvent message, List<String> messageArgs) {
         if (!message.getMessageAuthor().isBotOwner()) return;
+
+        if (messageArgs.size() == 0) { // Send a typing event if no message is specified
+            message.deleteMessage();
+            message.getChannel().type();
+            return;
+        }
+
         message.getMessage().getReferencedMessage().ifPresentOrElse(replyMessage -> { // Reply to the replied-to message if the invoking message was in reply to another.
             message.deleteMessage();
             replyMessage.reply(String.join(" ", messageArgs));
--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,6 +1,6 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "1.4.8";
-    public static final String BUILD_NUMBER = "10D120";
+    public static final String VERSION = "1.4.9";
+    public static final String BUILD_NUMBER = "10D121";
 }
