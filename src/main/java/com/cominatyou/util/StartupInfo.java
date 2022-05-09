3d5dde4c07f098dea917ac9ae2680eae1f5437ec
William M <cominatyou@cominatyou.com>
2022-05-08 19:13:20 -0500

    Add logging dependencies

--- src/main/java/com/cominatyou/util/StartupInfo.java
+++ src/main/java/com/cominatyou/util/StartupInfo.java
@@ -8,13 +8,13 @@ import org.javacord.api.entity.permission.Permissions;
 
 public class StartupInfo {
     public static void output(DiscordApi client) {
-        System.out.printf("\nHildabot %s (%s)\n", Version.VERSION, Version.BUILD_NUMBER);
+        System.out.printf("Hildabot %s (%s)\n", Version.VERSION, Version.BUILD_NUMBER);
+
+        System.out.printf("Logged in as %s\n", client.getYourself().getDiscriminatedName());
+        System.out.println("Invite bot with " + new BotInviteBuilder(client.getClientId()).setPermissions(Permissions.fromBitmask(412652792896L)).build() + "\n");
 
         if (!System.getProperty("os.name").equalsIgnoreCase("Linux")) {
             System.out.println("\u001B[33mHost is not running Linux, assuming this is a testing environment.\u001B[0m");
         }
-
-        System.out.printf("Logged in as %s\n", client.getYourself().getDiscriminatedName());
-        System.out.println("Invite bot with " + new BotInviteBuilder(client.getClientId()).setPermissions(Permissions.fromBitmask(412652792896L)).build() + "\n");
     }
 }
--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,6 +1,6 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "1.5.7";
-    public static final String BUILD_NUMBER = "10E133";
+    public static final String VERSION = "1.6.0";
+    public static final String BUILD_NUMBER = "10F134";
 }
