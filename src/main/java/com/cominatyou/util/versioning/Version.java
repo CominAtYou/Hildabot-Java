5e5035988014ed32697e8d7bf47c255a7367472c
William M <cominatyou@cominatyou.com>
2022-09-11 00:41:35 -0500

    Bump version number

--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,7 +1,7 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "1.14.1";
-    public static final String BUILD_NUMBER = "10I185";
+    public static final String VERSION = "1.14.2";
+    public static final String BUILD_NUMBER = "10I186";
     public static final String VERSION_STRING = String.format("%s (%s)", VERSION, BUILD_NUMBER);
 }
