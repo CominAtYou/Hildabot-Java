0a242de92407d64534f979b32b9bfa4f32f4ad06
William M <cominatyou@cominatyou.com>
2022-12-25 01:49:40 -0600

    Use TIMEZONE_AMERICA_CHICAGO for RoutineTasks

--- src/main/java/com/cominatyou/routinetasks/RoutineTasks.java
+++ src/main/java/com/cominatyou/routinetasks/RoutineTasks.java
@@ -11,6 +11,8 @@ import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
 import org.quartz.impl.StdSchedulerFactory;
 
+import com.cominatyou.util.Values;
+
 public class RoutineTasks {
     /**
      * Schedule the routine tasks that run every day at midnight Central Time.
@@ -21,8 +23,8 @@ public class RoutineTasks {
         final JobDetail birthdayJob = JobBuilder.newJob(CheckForBirthdays.class).withIdentity("birthdayAnnoucement", "routineTasks").build();
         final JobDetail streakJob = JobBuilder.newJob(SendStreakWarning.class).withIdentity("streakwarning", "routineTasks").build();
 
-        final Trigger birthdayTrigger = TriggerBuilder.newTrigger().withIdentity("birthdayTrigger", "routineTasks").withSchedule(CronScheduleBuilder.cronSchedule("0 0 0 * * ?").inTimeZone(TimeZone.getTimeZone("America/Chicago"))).build();
-        final Trigger streakTrigger = TriggerBuilder.newTrigger().withIdentity("streakTrigger", "routineTasks").withSchedule(CronScheduleBuilder.cronSchedule("0 0 0 * * ?").inTimeZone(TimeZone.getTimeZone("America/Chicago"))).build();
+        final Trigger birthdayTrigger = TriggerBuilder.newTrigger().withIdentity("birthdayTrigger", "routineTasks").withSchedule(CronScheduleBuilder.cronSchedule("0 0 0 * * ?").inTimeZone(TimeZone.getTimeZone(Values.TIMEZONE_AMERICA_CHICAGO))).build();
+        final Trigger streakTrigger = TriggerBuilder.newTrigger().withIdentity("streakTrigger", "routineTasks").withSchedule(CronScheduleBuilder.cronSchedule("0 0 0 * * ?").inTimeZone(TimeZone.getTimeZone(Values.TIMEZONE_AMERICA_CHICAGO))).build();
 
         try {
             final Scheduler scheduler = new StdSchedulerFactory().getScheduler();
--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -2,6 +2,6 @@ package com.cominatyou.util.versioning;
 
 public class Version {
     public static final String VERSION = "1.15.4";
-    public static final String BUILD_NUMBER = "10J195";
+    public static final String BUILD_NUMBER = "10J196";
     public static final String VERSION_STRING = String.format("%s (%s)", VERSION, BUILD_NUMBER);
 }
