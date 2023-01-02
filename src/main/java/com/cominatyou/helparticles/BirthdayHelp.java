cac7b2c08243050268a70a029d0cac14d8f8bc7b
William M <cominatyou@cominatyou.com>
2023-01-01 21:37:07 -0500

    Add extra newline to subcommands text

--- src/main/java/com/cominatyou/helparticles/BirthdayHelp.java
+++ src/main/java/com/cominatyou/helparticles/BirthdayHelp.java
@@ -10,7 +10,7 @@ public class BirthdayHelp {
     public static final EmbedBuilder ARTICLE_EMBED = new EmbedBuilder()
         .setColor(Values.HILDA_BLUE)
         .setTitle("Birthdays")
-        .setDescription("Allows you to register your birthday with the bot to have your birthday announced in <#609253148564914187>, as well as receiving a nifty role for the duration of your birthday!\n**Subcommands:**")
+        .setDescription("Allows you to register your birthday with the bot to have your birthday announced in <#609253148564914187>, as well as receiving a nifty role for the duration of your birthday!\n\n**Subcommands:**")
         .addField("Set", "Set your birthday to a certain day. Specify your birthday in `mm-dd` format, prepending any number with 0 if it is less than 10.")
         .addField("Edit", "Edit your birthday. The syntax is identical to the `set` subcommand.")
         .addField("List", "List birthdays for a certain month. Specify the month with the number representing the month, or omit it to get birthdays for this month.")
--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,7 +1,7 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "1.15.6";
-    public static final String BUILD_NUMBER = "10J200";
+    public static final String VERSION = "1.15.7";
+    public static final String BUILD_NUMBER = "10J201";
     public static final String VERSION_STRING = String.format("%s (%s)", VERSION, BUILD_NUMBER);
 }
