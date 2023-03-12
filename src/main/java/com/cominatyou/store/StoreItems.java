a7f767d96b307de876bea86476a7fee8b91b7d79
William M <cominatyou@cominatyou.com>
2023-03-12 00:12:00 -0600

    Fix description for medium submit boosts

--- src/main/java/com/cominatyou/store/StoreItems.java
+++ src/main/java/com/cominatyou/store/StoreItems.java
@@ -9,7 +9,7 @@ public class StoreItems {
     private static final List<StoreItem> items = List.of(
         new SmallXPIncrease("50 XP", "Instantly get an extra 50 XP!", null, "smallxp", 150, PurchaseLimit.UNLIMITED),
         new MediumXPIncrease("250 XP", "Instantly get an extra 250 XP!", null, "mediumxp", 300, PurchaseLimit.UNLIMITED),
-        new MediumSubmitBoost("3 Medium Submission Boosts", "Increase the XP you receive for your next submission by 10%.", "This item can only be purchased once every seven days.", "mediumsubmissionboost", 200, PurchaseLimit.WEEKLY),
+        new MediumSubmitBoost("3 Medium Submission Boosts", "Increase the XP you receive for your next 3 submissions by 10%.", "This item can only be purchased once every seven days.", "mediumsubmissionboost", 200, PurchaseLimit.WEEKLY),
         new LargeSubmitBoost("3 Large Submission Boosts", "Increase the XP you receive for your next 3 submissions by 25%.", "This item can only be purchased once every seven days.", "largesubmissionboost", 400, PurchaseLimit.WEEKLY)
     );
 
@@ -27,4 +27,3 @@ public class StoreItems {
         return null;
     }
 }
-
--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,7 +1,7 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "2.1.0";
-    public static final String BUILD_NUMBER = "11B005";
+    public static final String VERSION = "2.1.1";
+    public static final String BUILD_NUMBER = "11B006";
     public static final String VERSION_STRING = String.format("%s (%s)", VERSION, BUILD_NUMBER);
 }
