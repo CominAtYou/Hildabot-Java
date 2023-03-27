879ac4c379f46ea02d59069cadeadf25548b3c4c
William M <cominatyou@cominatyou.com>
2023-03-26 20:50:45 -0500

    Sort store items by price

--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,7 +1,7 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "2.1.3";
-    public static final String BUILD_NUMBER = "11B008";
+    public static final String VERSION = "2.1.4";
+    public static final String BUILD_NUMBER = "11B009";
     public static final String VERSION_STRING = String.format("%s (%s)", VERSION, BUILD_NUMBER);
 }
