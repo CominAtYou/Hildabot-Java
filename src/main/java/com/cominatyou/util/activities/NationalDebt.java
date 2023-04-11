3b235a972e7d04055550ee38efcd749c7e929abd
William M <cominatyou@cominatyou.com>
2023-04-11 10:30:37 -0500

    Move NationalDebt from HttpComponents to OkHttp

--- src/main/java/com/cominatyou/util/activities/NationalDebt.java
+++ src/main/java/com/cominatyou/util/activities/NationalDebt.java
@@ -1,46 +1,40 @@
 package com.cominatyou.util.activities;
 
-import org.apache.http.StatusLine;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.activity.ActivityType;
 import org.json.JSONObject;
 
-import java.nio.charset.StandardCharsets;
+import com.cominatyou.util.logging.Log;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 public class NationalDebt {
+    private static final OkHttpClient httpClient = new OkHttpClient();
     protected static void setNationalDebtStatus(DiscordApi client) {
-        CloseableHttpResponse response;
-        final CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        final Request request = new Request.Builder().url("https://www.treasurydirect.gov/NP_WS/debt/current?format=json").build();
 
-        try {
-            final HttpGet request = new HttpGet("https://www.treasurydirect.gov/NP_WS/debt/current?format=json");
-            response = httpClient.execute(request);
-        }
-        catch (Exception e) {
-            System.err.println("[NationalDebtActivity] Couldn't get the current national debt.");
-            return;
-        }
-
+        Response response;
         String responseBody;
         try {
-            responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+            response = httpClient.newCall(request).execute();
+            responseBody = response.body().string();
         }
-        catch (Exception e) {
-            System.err.println("[NationalDebtActivity] Couldn't get the current national debt.");
+        catch (IOException e) {
+            Log.error("NationalDebtActivity", "Couldn't get the national debt JSON.");
             return;
         }
 
-        final StatusLine statusLine = response.getStatusLine();
-        if (statusLine.getStatusCode() != 200) {
-            System.err.println("[NationalDebtActivity] Couldn't get the current national debt.");
+        if (!response.isSuccessful()) {
+            Log.error("NationalDebtActivity", "Couldn't get the national debt JSON.");
             return;
         }
+
+        response.close();
 
         JSONObject resultObject;
         try {
@@ -55,13 +49,5 @@ public class NationalDebt {
         final String debtString = new DecimalFormat("###,###.##").format(debt);
 
         client.updateActivity(ActivityType.PLAYING, String.format("the current national debt of the United States is $%s", debtString));
-
-        try {
-            httpClient.close();
-            response.close();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
