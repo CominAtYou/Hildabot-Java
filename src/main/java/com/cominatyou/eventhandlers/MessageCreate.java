f8b40e8da71f9094755888d998317ea80b842794
William M <cominatyou@cominatyou.com>
2023-04-18 22:31:03 -0500

    Permit XP messages as long as they're in a guild

--- src/main/java/com/cominatyou/eventhandlers/MessageCreate.java
+++ src/main/java/com/cominatyou/eventhandlers/MessageCreate.java
@@ -6,7 +6,6 @@ import com.cominatyou.util.MessageValidity;
 import com.cominatyou.util.Values;
 import com.cominatyou.xp.XPSystem;
 
-import org.javacord.api.entity.channel.ChannelType;
 import org.javacord.api.entity.message.MessageType;
 import org.javacord.api.event.message.MessageCreateEvent;
 
@@ -15,7 +14,7 @@ public class MessageCreate {
         if (!System.getProperty("os.name").equalsIgnoreCase("Linux") && !event.getMessageAuthor().isBotOwner()) return;
 
         if (!MessageValidity.test(event)) return;
-        if (!event.getMessageContent().startsWith(Config.PREFIX) && event.getChannel().getType() == ChannelType.SERVER_TEXT_CHANNEL && event.getServer().get().getId() == Values.BASE_GUILD_ID && (event.getMessage().getType() == MessageType.NORMAL || event.getMessage().getType() == MessageType.REPLY)) {
+        if (!event.getMessageContent().startsWith(Config.PREFIX) && event.getServer().isPresent() && event.getServer().get().getId() == Values.BASE_GUILD_ID && (event.getMessage().getType() == MessageType.NORMAL || event.getMessage().getType() == MessageType.REPLY)) {
             XPSystem.giveXPForMessage(event);
         }
         else {
