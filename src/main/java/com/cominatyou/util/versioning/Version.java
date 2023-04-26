a0499c224ec25754a689e3878484bf97959fce63
William M <cominatyou@cominatyou.com>
2023-04-26 14:31:18 -0500

    Revert "Use concatenation for VERSION_STRING"
    This reverts commit f6beb4fdade69a1dcab132893bc4035f840de768.

--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,7 +1,7 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "2.1.7";
-    public static final String BUILD_NUMBER = "11B012";
-    public static final String VERSION_STRING = VERSION + " " + BUILD_NUMBER;
+    public static final String VERSION = "2.1.6";
+    public static final String BUILD_NUMBER = "11B011";
+    public static final String VERSION_STRING = String.format("%s (%s)", VERSION, BUILD_NUMBER);
 }
