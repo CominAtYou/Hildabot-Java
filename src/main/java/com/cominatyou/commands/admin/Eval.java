a1090894f0a9b9eb4fc148196c46040161cf648d
William M <cominatyou@cominatyou.com>
2023-07-31 22:02:06 -0500

    Add eval command

---
+++ src/main/java/com/cominatyou/commands/admin/Eval.java
@@ -0,0 +1,41 @@
+﻿package com.cominatyou.commands.admin;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import org.javacord.api.event.message.MessageCreateEvent;
+
+import com.cominatyou.TextCommand;
+
+public class Eval extends TextCommand {
+    public void execute(MessageCreateEvent message, List<String> messageArgs) {
+        if (!message.getMessageAuthor().isBotOwner()) return;
+
+        final Process process;
+        boolean successful = false;
+        try {
+            final ProcessBuilder processBuilder = new ProcessBuilder("node" ,"-e", "console.log(eval(\"" + String.join(" ", messageArgs) +"\"));");
+            process = processBuilder.start();
+            successful = process.waitFor(10, TimeUnit.SECONDS);
+        }
+        catch (Exception e) {
+            message.getChannel().sendMessage("Failed to start the Node.js process.");
+            return;
+        }
+
+        if (!successful) {
+            message.getChannel().sendMessage("The Node.js process timed out.");
+        }
+        else if (process.exitValue() != 0) {
+            final String stderr = new BufferedReader(new InputStreamReader(process.getErrorStream())).lines().parallel().collect(Collectors.joining("\n"));
+            message.getChannel().sendMessage("The Node.js process exited with code " + process.exitValue() + ".\n```\n" + stderr + "\n```");
+        }
+        else {
+            final String output = new BufferedReader(new InputStreamReader(process.getInputStream())).lines().parallel().collect(Collectors.joining("\n"));
+            message.getChannel().sendMessage("```\n" + output + "\n```");
+        }
+    }
+}
--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,7 +1,7 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "2.1.13";
-    public static final String BUILD_NUMBER = "11B018";
+    public static final String VERSION = "2.2.0";
+    public static final String BUILD_NUMBER = "11C019";
     public static final String VERSION_STRING = String.format("%s (%s)", VERSION, BUILD_NUMBER);
 }
