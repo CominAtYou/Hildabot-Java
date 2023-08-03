6e8060a00466a2b336f9afd5053d790652d94d07
William M <cominatyou@cominatyou.com>
2023-08-03 11:38:35 -0500

    Don't send code block if stderr is empty

--- src/main/java/com/cominatyou/commands/admin/Eval.java
+++ src/main/java/com/cominatyou/commands/admin/Eval.java
@@ -31,7 +31,7 @@ public class Eval extends TextCommand {
         }
         else if (process.exitValue() != 0) {
             final String stderr = new BufferedReader(new InputStreamReader(process.getErrorStream())).lines().parallel().collect(Collectors.joining("\n"));
-            message.getChannel().sendMessage("The Node.js process exited with code " + process.exitValue() + ".\n```\n" + stderr + "\n```");
+            message.getChannel().sendMessage("The Node.js process exited with code " + process.exitValue() + "." + (stderr.isEmpty() ? "" : "```\n" + stderr + "\n```"));
         }
         else {
             final String output = new BufferedReader(new InputStreamReader(process.getInputStream())).lines().parallel().collect(Collectors.joining("\n"));
--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,7 +1,7 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "2.2.0";
-    public static final String BUILD_NUMBER = "11C019";
+    public static final String VERSION = "2.2.1";
+    public static final String BUILD_NUMBER = "11C020";
     public static final String VERSION_STRING = String.format("%s (%s)", VERSION, BUILD_NUMBER);
 }
