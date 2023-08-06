2db81abf4a7dcde76102356c80bf992d13d8022f
William M <cominatyou@cominatyou.com>
2023-08-06 01:26:49 -0500

    Revert "Suggest using ticket channel for assistance"
    This reverts commit 984f08dc0c5812fd97cd2306400bb1c521a3ad44.

--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,7 +1,7 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "2.2.3";
-    public static final String BUILD_NUMBER = "11C022";
+    public static final String VERSION = "2.2.2";
+    public static final String BUILD_NUMBER = "11C021";
     public static final String VERSION_STRING = String.format("%s (%s)", VERSION, BUILD_NUMBER);
 }
