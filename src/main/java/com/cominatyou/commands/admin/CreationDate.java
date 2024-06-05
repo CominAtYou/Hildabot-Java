1b6c651fc2ac21c4dead98a983e5c6c86a024a24
William M <cominatyou@cominatyou.com>
2024-06-05 23:21:31 +0800

    Update CreationDate regex

--- src/main/java/com/cominatyou/commands/admin/CreationDate.java
+++ src/main/java/com/cominatyou/commands/admin/CreationDate.java
@@ -9,7 +9,7 @@ import com.cominatyou.TextCommand;
 public class CreationDate extends TextCommand {
     public void execute(MessageCreateEvent message, List<String> messageArgs) {
         if (!message.getMessageAuthor().isBotOwner()) return;
-        if (messageArgs.size() < 1 && !messageArgs.get(0).matches("[0-9]{17,18}")) return;
+        if (messageArgs.size() < 1 && !messageArgs.get(0).matches("[0-9]{17,}")) return;
 
         final long id = Long.parseLong(messageArgs.get(0));
 
