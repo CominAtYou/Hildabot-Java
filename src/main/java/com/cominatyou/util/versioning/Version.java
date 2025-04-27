891c13b53b9a8fb8d9d85c8e5d16e2d9480f71a9
William M <cominatyou@cominatyou.com>
2025-04-26 19:02:01 -0500

    Use 0.75x streak for submit XP amounts

--- src/main/java/com/cominatyou/util/versioning/Version.java
+++ src/main/java/com/cominatyou/util/versioning/Version.java
@@ -1,7 +1,7 @@
 package com.cominatyou.util.versioning;
 
 public class Version {
-    public static final String VERSION = "2.7.2";
-    public static final String BUILD_NUMBER = "11H053";
+    public static final String VERSION = "2.7.3";
+    public static final String BUILD_NUMBER = "11H054";
     public static final String VERSION_STRING = String.format("%s (%s)", VERSION, BUILD_NUMBER);
 }
